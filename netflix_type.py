8a6c6773c239031cffc7922d7e6e2fbae24771a1
Jiaxin Ying <112274822+helenyjx@users.noreply.github.com>
2022-11-02 22:58:06 +0000

    final

--- netflix_type.py
+++ netflix_type.py
@@ -7,7 +7,7 @@ connects = sqlite3.connect('netflix_titles.db')
 cursor = connects.cursor()
 
 ## Step 2: Create a new table called netflix_type1
-new_table = '''CREATE TABLE netflix_type00(
+new_table = '''CREATE TABLE netflix_type000(
  				show_id VAR PRIMARY KEY,
  				type VAR,
  				title VAR,
@@ -25,7 +25,7 @@ new_table = '''CREATE TABLE netflix_type00(
 
 ## Step 3: Insert the new table into the original database, then insert data into table netflix_type by using SQL query
 cursor.execute(new_table)
-insert_data = "INSERT INTO netflix_type00 (show_id, type, title, director,cast,country,date_added,release_year,rating,duration,listed_in,description) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+insert_data = "INSERT INTO netflix_type000 (show_id, type, title, director,cast,country,date_added,release_year,rating,duration,listed_in,description) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
 ## Step 4: Read each record from netflix_titles.csv file and import its records to new table netflix_type
 file = open('netflix_titles.csv')
@@ -34,7 +34,7 @@ cursor.executemany(insert_data, records)
 
 ### Query 1: Find the top five countries who have the most TV Shows in Netlfix
 query1 = """SELECT Distinct country, count(type) 
-		   FROM netflix_type00
+		   FROM netflix_type000
 		   WHERE country != '' AND type='TV Show'
 		   Group by country
 		   Order by count(type) DESC
@@ -43,7 +43,7 @@ top5_tv_country = cursor.execute(query1).fetchall()
 
 ### Query 2: Find the top five countries who have the most movies in Netlfix
 query2 = """SELECT Distinct country, count(type) 
-		   FROM netflix_type00
+		   FROM netflix_type000
 		   WHERE country != '' AND type='Movie'
 		   Group by country
 		   Order by count(type) DESC
@@ -52,7 +52,7 @@ top5_movie_country = cursor.execute(query2).fetchall()
 
 ### Query 3: Find the top five directors who directed the most movies or TV shows in Netlfix
 query3 = """SELECT Distinct director, count(title) 
-		   FROM netflix_type00
+		   FROM netflix_type000
 		   WHERE director != '' 
 		   Group by director
 		   Order by count(title) DESC
@@ -61,7 +61,7 @@ top5_director = cursor.execute(query3).fetchall()
 
 ### Query 4: Find the top five directors who have the most movies or TV shows in Netlfix are listed in Documentaries
 query4 = """SELECT Distinct director, count(listed_in) 
-		   FROM netflix_type00
+		   FROM netflix_type000
 		   WHERE director != '' AND listed_in = 'Documentaries'
 		   Group by director
 		   Order by count(listed_in) DESC
@@ -70,7 +70,7 @@ top5_director_documentaries = cursor.execute(query4).fetchall()
 
 ### Query 5: Find the top five directors who have the most adult movies or TV shows in Netlfix
 query5 = """SELECT Distinct director, count(rating) 
-		   FROM netflix_type00
+		   FROM netflix_type000
 		   WHERE director != '' AND rating in ('R', 'TV-MA', 'NC-17')
 		   Group by director
 		   Order by count(rating) DESC
